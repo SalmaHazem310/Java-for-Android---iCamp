3994085616f9c6fa882b24e8d8e7eac22cf7a5cb
SalmaHazem310 <salmahazem310@yahoo.com>
2021-08-27 12:04:13 +0200

    Initial Commit

---
+++ Android/app/src/main/java/com/example/ihubandroid/fbPageConstraint.java
@@ -0,0 +1,14 @@
+package com.example.ihubandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fbPageConstraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fb_page_constraint);
+    }
+}
